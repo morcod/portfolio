fa97958a37aabc224c9895509d2ce82e7e9c50b4
barchah.nabil@ofppt-edu.ma <barchah.nabil@ofppt-edu.ma>
2021-02-09 16:25:31 +0100

    Initial commit add navbar

---
+++ src/App.js
@@ -0,0 +1,13 @@
+//components
+import  MyNavbar  from "./components/mynavbar/mynavbar.component";
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <MyNavbar/>
+    </div>
+  );
+}
+
+export default App;
